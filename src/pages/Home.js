b245472e854c1e06715d39a1e8d30a8b0a3908ac
Jackie Luc <jaluc@ucalgary.ca>
2018-07-08 17:05:14 -0600

    Update project structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './images/nldc2019-logo.png';
-import './App.css';
+import logo from '../images/nldc2019-logo.png';
+import '../styles/pages/Home.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
       <div className="App">
@@ -11,11 +11,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/pages/Home.js</code> and save to reload.
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default Home;
