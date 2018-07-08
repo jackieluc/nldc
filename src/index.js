412c8137edf0e2a7487c9c0878a2f9172e2d3d96
Jackie Luc <jaluc@ucalgary.ca>
2018-07-08 17:43:11 -0600

    Add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 import Home from './pages/Home';
-import registerServiceWorker from './registerServiceWorker';
+import Sponsors from './pages/Sponsors';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render((
+  <Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/sponsors' component={Sponsors} />
+    </Switch>
+  </Router>
+), document.getElementById('root'));
 registerServiceWorker();
---
+++ src/pages/Sponsors.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="sponsors">
+        <header className="sponsors-header">
+          <h1 className="sponsors-title">Welcome to React</h1>
+        </header>
+        <p className="sponsors-intro">
+          To get started, edit <code>src/pages/Sponsors.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
