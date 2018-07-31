6928e0bd4f7f9dd21ec82187eb6c3d33dd0ce200
Jackie Luc <jackieluc17@gmail.com>
2018-07-31 00:53:55 -0600

    Working transformation for hamburger menu (toggle open/close)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -31,20 +31,36 @@ const Line = styled.span`
   border-radius: 0.1rem;
   background: black;
   left: 27.5%;
-  top: ${props => props.line.topPosition}
+  top: ${props => props.styles.topPosition}
 
   ${media.laptop`
     height: 0.19rem;
   `}
+
+  &.closed {
+    ${props => {
+      if (props.styles.line === 1) {
+        return `transform: rotate(45deg);
+          top:50%;
+        `
+      }
+      else {
+        return `transform: rotate(-45deg);
+          top:50%;
+        `
+      }
+    }
+  }
 `;
 
 class Nav extends Component {
   render() {
+    const isMenuClosed = this.props.status.isMenuClosed ? 'closed' : null;
     return (
       <div className="nav-wrapper">
-        <Line line={{ topPosition: '35%' }} />
-        <Line line={{ topPosition: '50%' }} />
-        <Line line={{ topPosition: '65%' }} />
+        <Line styles={{ line: 1, topPosition: '35%' }} className={isMenuClosed}/>
+        <Line styles={{ line: 2, topPosition: '50%' }} className={isMenuClosed}/>
+        <Line styles={{ line: 3, topPosition: '65%' }} className={isMenuClosed}/>
       </div>
     );
   }
@@ -63,10 +79,24 @@ class Menu extends Component {
   }
 }
 
-export default function Navigation() {
-  return (
-    <NavButton>
-      <Nav />
-    </NavButton>
-  );
+export default class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isMenuClosed: false };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      isMenuClosed: !this.state.isMenuClosed,
+    });
+  }
+
+  render() {
+    return (
+      <NavButton onClick={this.onClick}>
+        <Nav status={{ isMenuClosed: this.state.isMenuClosed }}/>
+      </NavButton>
+    );
+  }
 }
