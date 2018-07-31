48099250d0152bb1b9d3984fa9110606dd948d2a
Jackie Luc <jackieluc17@gmail.com>
2018-07-31 00:21:40 -0600

    Add nav menu styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -24,6 +24,32 @@ const NavButton = styled.button`
   `}
 `;
 
+const Line = styled.span`
+  position: absolute;
+  width: 45%;
+  height: 0.11rem;
+  border-radius: 0.1rem;
+  background: black;
+  left: 27.5%;
+  top: ${props => props.line.topPosition}
+
+  ${media.laptop`
+    height: 0.19rem;
+  `}
+`;
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="nav-wrapper">
+        <Line line={{ topPosition: '35%' }} />
+        <Line line={{ topPosition: '50%' }} />
+        <Line line={{ topPosition: '65%' }} />
+      </div>
+    );
+  }
+}
+
 class Menu extends Component {
   render() {
     return (
@@ -39,6 +65,8 @@ class Menu extends Component {
 
 export default function Navigation() {
   return (
-    <NavButton />
+    <NavButton>
+      <Nav />
+    </NavButton>
   );
 }
