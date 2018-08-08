d95de1d5da36580ba3455ea3c867e4ab2cb02d86
Jackie Luc <jackieluc17@gmail.com>
2018-08-07 23:53:26 -0600

    DesktopNav - toggle between transparent and white navbar (sticky)

--- src/components/DesktopNavigation.js
+++ src/components/DesktopNavigation.js
@@ -10,7 +10,14 @@ const ResponsiveWrapper = styled.div`
 
 
 const MenuWrapper = styled.nav`
-  background-color: transparent;
+  ${props => props.heights.currentWindowHeight < props.heights.firstSectionHeight / 2
+    ? `background-color: transparent;
+      a { color: white; };
+    `
+    : `background-color: white;
+      a { color: #037EF3; };
+      box-shadow: 0 3px 6px rgba(75, 97, 141, 0.3);
+    `}
   height: 6vh;
   padding: 8px 16px;
   position: fixed;
@@ -20,20 +27,43 @@ const MenuWrapper = styled.nav`
   left: 0;
   right: 0;
   z-index: 3;
-`;
+  `;
+  
+  const StyledLink = styled(NavLink)`
+    font-size: 1.2rem;
+    padding: 8px 1.5rem;
+    letter-spacing: 1px;
+    text-decoration: none;
+  `;
+  class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentWindowHeight: 0, firstSectionHeight: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
 
-const StyledLink = styled(NavLink)`
-  color: white;
-  font-size: 1.2rem;
-  padding: 8px 1.5rem;
-  letter-spacing: 1px;
-  text-decoration: none;
-`;
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('scroll', this.updateWindowDimensions);
+  }
 
-class Menu extends Component {
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    const currentWindowHeight = document.documentElement.scrollTop;
+    const firstSectionHeight = document.getElementsByTagName('section')[0].clientHeight;
+    this.setState({ 
+      currentWindowHeight,
+      firstSectionHeight
+    });
+  }
+
   render() {
+    const { currentWindowHeight, firstSectionHeight } = this.state;
     return (
-      <MenuWrapper>
+      <MenuWrapper heights={{ currentWindowHeight, firstSectionHeight }}>
         <StyledLink to="/" onClick={this.props.action}>Home</StyledLink>
         <StyledLink to="/sponsors"onClick={this.props.action}>Sponsors</StyledLink>
         <StyledLink to="/team"onClick={this.props.action}>Team</StyledLink>
