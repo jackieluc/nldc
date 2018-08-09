1b4e882448411ef37a84ef49dc5e5976c8664aad
Jackie Luc <jackieluc17@gmail.com>
2018-08-08 23:26:47 -0600

    Utils/Media - Eslinted

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,23 +1,23 @@
 import { css } from 'styled-components';
 
 const BreakPoints = {
-    bigDesktop: 1800, 
-    desktop: 1200,
-    laptop: 1024,
-    tablet: 600,
-    phone: 320,
+  bigDesktop: 1800,
+  desktop: 1200,
+  laptop: 1024,
+  tablet: 600,
+  phone: 320,
 };
 
 // Iterate through the break point sizes and create a media template
 const Media = Object.keys(BreakPoints).reduce((acc, label) => {
-    acc[label] = (...args) => css`
-        @media (min-width: ${BreakPoints[label] / 16}em) {
-            ${css(...args)}
-        }`
-    return acc;
+  acc[label] = (...args) => css`
+    @media (min-width: ${BreakPoints[label] / 16}em) {
+        ${css(...args)}
+    }`;
+  return acc;
 }, {});
 
-export { 
-    Media,
-    BreakPoints
-}
+export {
+  Media,
+  BreakPoints,
+};
