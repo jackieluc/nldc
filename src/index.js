5600c74095615cfc6838e0af7b9197fdba0ee004
Jackie Luc <jackieluc17@gmail.com>
2018-08-08 23:38:33 -0600

    index.js - Eslinted

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
 import { injectGlobal } from 'styled-components';
+import registerServiceWorker from './registerServiceWorker';
 import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home';
 import Sponsors from './pages/Sponsors';
 import Team from './pages/Team';
 
+// eslint-disable-next-line
 injectGlobal`
   body {
     margin: 0;
@@ -30,27 +31,27 @@ injectGlobal`
 const routes = [
   {
     name: 'Home',
-    path: '/'
+    path: '/',
   },
   {
     name: 'Sponsors',
-    path: '/sponsors'
+    path: '/sponsors',
   },
   {
     name: 'Team',
-    path: '/team'
+    path: '/team',
   },
 ];
 
 ReactDOM.render((
   <Router>
     <main>
-      <Navigation routes={routes}/>
+      <Navigation routes={routes} />
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path="/" component={Home} />
         {/* <Route exact path='/about-us' component={AboutUs} /> */}
-        <Route exact path='/sponsors' component={Sponsors} />
-        <Route exact path='/team' component={Team} />
+        <Route exact path="/sponsors" component={Sponsors} />
+        <Route exact path="/team" component={Team} />
         {/* <Route exact path='/schedule' component={Schedule} /> */}
         {/* <Route exact path='/events' component={Events} /> */}
         {/* <Route path="*" component={NotFoundPage} /> */}
