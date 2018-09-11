c1e8c71734d66e412302b15a8c66b8a426cc867f
Jackie Luc <jackie@MacBook.local>
2018-09-10 23:28:28 -0600

    Home - Add svg divider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Div from '../images/divider-right.svg';
 import Hero from '../components/Hero';
 import EventDetails from '../components/EventDetails';
 import SponsorDetails from '../components/SponsorDetails';
@@ -9,10 +10,18 @@ const App = styled.div`
   text-align: center;
 `;
 
+const Divider = styled.div`
+  background-image: url('${Div}');
+  width: 300px;
+  height: 34px;
+  margin: 0 auto;
+`;
+
 const Home = () => (
   <App>
     <Hero />
     <EventDetails />
+    <Divider />
     <SponsorDetails />
     <Footer />
   </App>
