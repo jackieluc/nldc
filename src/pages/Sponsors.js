d70756363c00956afd8ea409062d6d0bbf2821a8
Jackie Luc <jackieluc17@gmail.com>
2018-09-12 00:01:20 -0600

    Sponsors page - initial styling

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 5rem;
+const Wrapper = styled.div`
   text-align: center;
-  color: palevioletred;
 `;
 
-const SponsorSection = styled.section`
-  padding: 4em;
-  background: papayawhip;
+const Title = styled.h1`
+  background: #7F7FD5;
+  background: -webkit-linear-gradient(to right, #9BCDFF, #7F7FD5);
+  background: linear-gradient(to right, #9BCDFF, #7F7FD5);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 2.5rem;
+`;
+
+const TitleSection = styled.section`
+  margin-top: 3rem;
+  margin-bottom: 0;
 `;
 
 const Sponsors = () => (
-  <div className="about">
-    <SponsorSection>
+  <Wrapper>
+    <TitleSection>
       <Title>
         Sponsors
       </Title>
-    </SponsorSection>
-    <p className="section about-intro">
+    </TitleSection>
+    <p>
       { // eslint-disable-next-line
       } In order to have a successful NLDC, we require funding for venues, events, speakers, and much more. We are looking to form valuable partnerships with organizations that value leadership, diversity, innovation, technology, and entrepreneurial thinking. NLDC is the perfect occasion for your organization to promote the rich opportunities that your organization has to offer to some of Canada's next generation of business leaders.
     </p>
-  </div>
+  </Wrapper>
 );
 
 export default Sponsors;
