a8b24338bb04387b932118769bd7b518cdc69fd0
Jackie Luc <jackieluc17@gmail.com>
2018-10-07 22:48:35 -0600

    Sponsors page + benefits

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  Container, Row, Col, CardDeck,
+} from 'reactstrap';
 import SponsorPackage from '../components/SponsorPackage/SponsorPackage';
 
 const Wrapper = styled.div`
@@ -13,6 +16,7 @@ const Title = styled.h1`
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   font-size: 2.5rem;
+  margin-bottom: 15px;
 `;
 
 const TitleSection = styled.section`
@@ -20,19 +24,46 @@ const TitleSection = styled.section`
   margin-bottom: 0;
 `;
 
+const Description = styled(Col)`
+  text-align: center;
+  margin: 0 auto 15px auto;
+`;
+
+const benefits1 = ['Keynote Presentations', 'Corporate Videos', 'Email Blast to Delegates', 'Plenary Recognition', 'Logo Exposure', 'Conference Website', 'Merchandise', 'Delegate Package', 'Snapchat Filters', 'Social Media Promotion', 'Promotional Products Distributed'];
+
+const benefits2 = ['Conference Boothing Table', 'Complimentary Banquet Tickets', 'External Speaking Opportunities', 'Innovation Series Workshops', 'Recruitment Materials Distributed'];
+
+const benefits3 = ['International Recruitment Program Discount'];
+
 const Sponsors = () => (
   <Wrapper>
-    <TitleSection>
-      <Title>
-        Sponsors
-      </Title>
-    </TitleSection>
-    <p>
-      { // eslint-disable-next-line
-      } In order to have a successful NLDC, we require funding for venues, events, speakers, and much more. We are looking to form valuable partnerships with organizations that value leadership, diversity, innovation, technology, and entrepreneurial thinking. NLDC is the perfect occasion for your organization to promote the rich opportunities that your organization has to offer to some of Canada's next generation of business leaders.
-    </p>
-    <SponsorPackage />
-    <SponsorPackage />
+    <Container>
+      <TitleSection>
+        <Title>
+          Sponsors
+        </Title>
+        <Row>
+          <Description xs="12" lg="8">
+            <p>
+              { // eslint-disable-next-line
+              } In order to have a successful NLDC, we require funding for venues, events, speakers, and much more. We are looking to form valuable partnerships with organizations that value leadership, diversity, innovation, technology, and entrepreneurial thinking. NLDC is the perfect occasion for your organization to promote the rich opportunities that your organization has to offer to some of Canada's next generation of business leaders.
+            </p>
+          </Description>
+        </Row>
+      </TitleSection>
+      <section>
+        <Title>
+          Benefits
+        </Title>
+        <Row>
+          <CardDeck style={{ width: '100%' }}>
+            <SponsorPackage title="Branding & Recognition" benefits={benefits1} />
+            <SponsorPackage title="Networking & Recruitment" benefits={benefits2} />
+            <SponsorPackage title="Corporate & Social Responsibility" benefits={benefits3} />
+          </CardDeck>
+        </Row>
+      </section>
+    </Container>
   </Wrapper>
 );
 
