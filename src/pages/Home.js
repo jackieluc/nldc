b6cff71aa284410ff078c7531ba17deef05740b8
Jackie Luc <jackieluc17@gmail.com>
2018-10-18 02:43:30 -0600

    Refactor Divider to a separate component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Div from '../images/divider-right.svg';
+import Divider from '../components/Dividers/RightDivider';
 import Hero from '../components/Hero';
 import EventDetails from '../components/EventDetails';
 import SponsorDetails from '../components/SponsorDetails';
@@ -8,13 +8,6 @@ import Footer from '../components/Footer';
 
 const Wrapper = styled.div`
   text-align: center;
-`;
-
-const Divider = styled.div`
-  background-image: url('${Div}');
-  width: 280px;
-  height: 34px;
-  margin: 1rem auto;
 `;
 
 const Home = () => (
