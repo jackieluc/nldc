898e4d4053bc31932fcbc380f11f40890ab55dea
Jackie Luc <jackieluc17@gmail.com>
2018-10-18 03:06:48 -0600

    Sponsors page refresh part 1

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -3,16 +3,9 @@ import styled from 'styled-components';
 import {
   Container, Row, Col, CardDeck,
 } from 'reactstrap';
-import Div from '../images/divider-right.svg';
-
+import Wave from '../components/Dividers/Wave';
+import styles from '../utils/styles';
 import SponsorPackage from '../components/SponsorPackage/SponsorPackage';
-
-const Divider = styled.div`
-  background-image: url('${Div}');
-  width: 280px;
-  height: 34px;
-  margin: 1rem auto;
-`;
 
 const Wrapper = styled.div`
   text-align: center;
@@ -20,8 +13,8 @@ const Wrapper = styled.div`
 
 const Title = styled.h1`
   background: #7F7FD5;
-  background: -webkit-linear-gradient(to right, #9BCDFF, #7F7FD5);
-  background: linear-gradient(to right, #9BCDFF, #7F7FD5);
+  background: -webkit-linear-gradient(to right, ${styles.themeColour}, #7F7FD5);
+  background: linear-gradient(to right, ${styles.themeColour}, #7F7FD5);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   font-size: 2.5rem;
@@ -30,12 +23,32 @@ const Title = styled.h1`
 
 const TitleSection = styled.section`
   margin-top: 4rem;
-  margin-bottom: 1rem;
+  margin-bottom: 2rem;
 `;
 
 const Description = styled(Col)`
   text-align: center;
   margin: 0 auto;
+`;
+
+const Benefits = styled.section`
+  background-color: ${styles.themeColour};
+  width: 100%;
+  padding: 2rem 0 6rem 0;
+  margin-bottom: 0;
+`;
+
+const BenefitsTitle = styled.h1`
+  color: #FFFFFF;
+  font-size: 2.5rem;
+  margin-bottom: 2rem;
+`;
+
+const Values = styled(Container)`
+  padding-top: 4rem; 
+  background-color: #F4F7F8;
+  position: relative;
+  z-index: 2;
 `;
 
 const benefits1 = ['Keynote presentations', 'Corporate videos', 'Plenary recognition', ['Logo exposure', 'Conference website', 'Merchandise', 'Delegate package'], 'Snapchat filters', 'Social media promotion', 'Promotional products'];
@@ -64,23 +77,42 @@ const Sponsors = () => (
           </Description>
           <Col xs="12">
             <a href="mailto:jacoby.holder@aiesec.net?subject=Sponsorship Inquiry">
-              Become a sponsor today!
+              Become our partner today!
             </a>
           </Col>
         </Row>
       </TitleSection>
     </Container>
-    <Divider />
-    <section style={{ backgroundColor: '#F7F7F7', width: '100%', padding: '2rem 0' }}>
-      <Title>
-          Benefits
-      </Title>
+    <Benefits>
+      <BenefitsTitle>
+        Benefits
+      </BenefitsTitle>
       <CardDeck>
         <SponsorPackage title="Branding & Recognition" benefits={benefits1} />
         <SponsorPackage title="Networking & Recruitment" benefits={benefits2} />
         <SponsorPackage title="CSR" benefits={benefits3} />
       </CardDeck>
-    </section>
+    </Benefits>
+    <Wave />
+    <Values fluid>
+      <Row>
+        <Description xs="12" md="9">
+          <p>
+            { // eslint-disable-next-line
+            } For over 65 years, we have worked alongside government, businesses and talented  students to facilitate life changing professional experiences within hundreds of small to multinational businesses. We have provided talented youth with meaningful and challenging experiences to develop their leadership potential in Canada.
+          </p>
+          <div>
+            Please visit
+            {' '}
+            <a href="https://www.aiesec.ca/hire-global-talent/" target="_blank" rel="noopener noreferrer">
+              https://www.aiesec.ca/hire-global-talent/
+            </a>
+            {' '}
+            for more information.
+          </div>
+        </Description>
+      </Row>
+    </Values>
   </Wrapper>
 );
 
