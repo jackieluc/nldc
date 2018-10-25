734c984b5580594502d8f8492094b1004a70ee01
Jackie Luc <jackieluc17@gmail.com>
2018-10-24 21:36:53 -0600

    Footer - basic footer setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Container } from 'reactstrap';
 import MailChimp from './MailChimp/MailChimp';
-// import styled from 'styled-components';
+import Media from '../utils/media';
 // import Media from '../utils/media';
 
+const Wrapper = styled.footer`
+  background-color: #F9F9F9;
+  margin: 0;
+  padding-top: 80px;
+  padding-bottom: 60px;
+  ${Media.laptop`padding-bottom: 0;`}
+`;
+
+
 const Footer = () => (
-  <section>
-    <p>
-      For any inquiries, please contact Jake Holder at
-      {' '}
-      <a href="mailto:jacoby.holder@aiesec.net?subject=Sponsorship Inquiry">
-        jacoby.holder@aiesec.net
-      </a>
-    </p>
-    <MailChimp />
-  </section>
+  <Wrapper>
+    <Container>
+      <MailChimp />
+    </Container>
+  </Wrapper>
 );
 
 export default Footer;
