044492bfe7368d2964e09656181421835b8c0ea2
Jackie Luc <jackieluc17@gmail.com>
2018-10-25 01:01:17 -0600

    AboutAisec - clean up

--- src/components/About/AboutAiesec.js
+++ src/components/About/AboutAiesec.js
@@ -2,33 +2,40 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import styled from 'styled-components';
 import Styles from '../../utils/styles';
+import Media from '../../utils/media';
 
 const About = styled.div`
-color: white;
+  color: white;
 `;
 
 const Title = styled.h2`
-margin-bottom: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const Text = styled.p`
+  text-align: left;
+  ${Media.tablet`text-align: center;`}
 `;
 
 const CTA = styled.a`
-color: white;
-display: block;
-padding-top: 2rem;
-
-&:hover {
   color: white;
-  text-decoration: underline;
-}
+  display: block;
+  padding-top: 2rem;
+  margin-left: -25px;
 
-&:after {
-  content: "→";
-  display: inline-block;
-  margin-left: .236em;
-  margin-right: -1.618em;
-  position: relative;
-  top: .01em;
-}
+  &:hover {
+    color: white;
+    text-decoration: underline;
+  }
+
+  &:after {
+    content: "→";
+    display: inline-block;
+    margin-left: .236em;
+    margin-right: -1.618em;
+    position: relative;
+    top: .01em;
+  }
 `;
 
 const AboutAiesec = () => (
@@ -39,14 +46,14 @@ const AboutAiesec = () => (
           <Title>
             About AIESEC
           </Title>
-          <p>
+          <Text>
             { // eslint-disable-next-line
             } AIESEC is the largest  non-governmental not-for-profit organisation offering platforms for young people to explore and develop their leadership through our global exchange program. Since we were founded, we have engaged and developed over 1,000,000 young people who have been through an AIESEC experiences.
-          </p>
-          <p>
+          </Text>
+          <Text>
             { // eslint-disable-next-line
             } We are in consultative status with the United Nations Economic and Social Council (ECOSOC), affiliated with the UN DPI, member of ICMYO, and is recognized by UNESCO.
-          </p>
+          </Text>
           <CTA href="https://aiesec.org/about-us" target="_blank" rel="noopener noreferrer">
             Find out more about AIESEC
           </CTA>
