45f7c42ce6e9744ca39c66a81f9b81f1377253ba
Jackie Luc <jackieluc17@gmail.com>
2018-10-24 21:36:40 -0600

    Add MailChimp (no styles)

--- src/components/MailChimp/MailChimp.js
+++ src/components/MailChimp/MailChimp.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const MailChimp = () => (
-  <div
-    id="mc_embed_signup"
-    style={{
-      background: '#fff',
-      clear: 'left',
-      font: '14px Helvetica,Arial,sans-serif',
-      width: '100%',
-    }}
-  >
-    <form action="https://nldcnow.us19.list-manage.com/subscribe/post?u=fe2fa79735e47235c75788bce&amp;id=fb15ca4f23" method="post" id="subscribe-form" name="subscribe-form" className="validate" target="_blank" noValidate>
-      <div>
-        <input type="email" value="" name="email" placeholder="you@example.com" required />
-        <input type="hidden" name="b_fe2fa79735e47235c75788bce_fb15ca4f23" tabIndex="-1" value="" aria-hidden="true" />
-        <button type="submit" name="subscribe">
-          Get Updates
-        </button>
-      </div>
-    </form>
-  </div>
-);
-
-export default MailChimp;
+export default function MailChimp() {
+  return (
+    <div id="mc_embed_signup">
+      <form
+        action="//nldcnow.us19.list-manage.com/subscribe/post?u=fe2fa79735e47235c75788bce&amp;id=fb15ca4f23"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        className="validate"
+        target="_blank"
+        noValidate
+      >
+        <label htmlFor="EMAIL">
+          Subscribe for Conference Updates!
+          <input
+            type="email"
+            name="EMAIL"
+            id="mce-EMAIL"
+            placeholder="email address"
+            required
+          />
+        </label>
+        <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+          <input type="text" name="b_fe2fa79735e47235c75788bce_fb15ca4f23" tabIndex="-1" value="" />
+        </div>
+        <input type="submit" value="Get Updates" name="subscribe" id="mc-embedded-subscribe" className="btn btn-primary" />
+      </form>
+    </div>
+  );
+}
