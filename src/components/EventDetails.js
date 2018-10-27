6063afaf99c1faabba0365bd7cc5dc833f8c5b11
Jackie Luc <jackieluc17@gmail.com>
2018-10-26 20:45:48 -0600

    EventDetails - redesign overhaul

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,111 +1,90 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import styled from 'styled-components';
 import Styles from '../utils/styles';
 import AddToCalendar from './Calendar/AddToCalendar';
 import Media from '../utils/media';
+import Motto from '../images/motto.svg';
 
-const EventDetailsSection = styled.section`
-  padding: 0 15px;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  ${Media.tablet`
-    padding: 10px 35px;
-  `}
-  ${Media.laptop`width: 80%;`}
-  ${Media.desktop`width: 1200px;`}
-`;
-
-const Column = styled.div`
-  box-sizing: border-box;
-  padding-left: 0;
-
-  ${Media.tablet`
-    flex: 0 50%;
-  `}
-`;
-
-const ColumnRight = styled(Column)`
+const ConferenceDescription = styled.div`
   width: 100%;
-
-  ${Media.tablet`
-    padding-left: 8%;
-    padding-top: 10px;
-  `}
 `;
 
-const EventTitle = styled.h2`
-  background: #7F7FD5;
-  background: -webkit-linear-gradient(to right, ${Styles.themeColour}, #7F7FD5);
-  background: linear-gradient(to right, ${Styles.themeColour}, #7F7FD5);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  font-size: 1.6rem;
-  ${Media.laptop`font-size: 2.3rem;`}
-  text-align: left;
-  margin: 10px auto;
-  text-transform: uppercase;
+const StyledRow = styled(Row)`
+  justify-content: center;
+  margin-bottom: 2rem;
+`;
+
+const MottoWrapper = styled.img`
+  margin: 0 auto 2rem auto;
+  ${Media.medPhone`width: 20rem;`}
+  ${Media.tablet`margin: 0 auto;`}
+  ${Media.laptop`width: 26rem;`}
+`;
+
+const DescriptionWrapper = styled(Col)`
+  display: flex;
+  align-items: center;
 `;
 
 const EventDescription = styled.p`
   text-align: left;
+  margin: 0;
 `;
 
-// const DescriptionWrapper = styled.span`
-//   font-size: 0.875rem;
-//   ${Media.laptop`font-size: 1rem;`}
-// `;
+const AddressLink = styled.a`
+  text-align: left;
+  display: block;
+`;
 
-// const AddressLink = styled.a`
-//   text-align: left;
-//   display: block;
-// `;
-
-const EventInfoTitle = styled(EventTitle)`
-  background: none;
-  -webkit-text-fill-color: ${Styles.textColour};
-  font-size: 2rem;
-  ${Media.tablet`font-size: 1.5rem;`}
-  ${Media.laptop`font-size: 1.75rem;`}
-  text-transform: none;
+const EventInfoTitle = styled.h3`
+  color: ${Styles.textColour};
+  text-align: left;
 `;
 
 const EventDetails = () => (
-  <EventDetailsSection>
-    <Wrapper>
-      <Column>
-        <EventTitle>
-            Disrupting the Now.
-        </EventTitle>
-        <EventDescription>
-          { // eslint-disable-next-line
-            } AIESEC Canada’s National Leadership Development Conference is an annual 5-day event that brings together 300 business leaders from 30+ universities across Canada to network and develop their skills in teamwork, sales, finance, and marketing. In 2019, we are introducing the Innovation Series to connect our delegates with the leading businesses in technology and disruption.
-        </EventDescription>
-      </Column>
-      <ColumnRight>
-        <EventInfoTitle>
-          When
-        </EventInfoTitle>
-        <EventDescription>
-          <AddToCalendar />
-        </EventDescription>
-        {/* <EventInfoTitle>
-            Where
-        </EventInfoTitle>
-        <EventDescription>
-          <DescriptionWrapper>
-            SAIT (Southern Alberta Institute of Technology), Calgary, Alberta
-            <AddressLink href="https://www.google.com/maps/place/SAIT/@51.0640269,-114.0907061,17z/data=!3m1!4b1!4m5!3m4!1s0x53716f9268a69e81:0xb1db5e68bd61e28!8m2!3d51.065755!4d-114.0897771" target="_blank" rel="noopener noreferrer">
-                1301-16 Avenue NW Calgary AB, T2M OL4
-            </AddressLink>
+  <section>
+    <Container>
+      <ConferenceDescription>
+        <StyledRow>
+          <Col xs="12" sm="10" lg="6">
+            <MottoWrapper src={Motto} alt="Disrupting the now." />
+          </Col>
+          <DescriptionWrapper xs="12" sm="10" lg="6">
+            <EventDescription>
+              { // eslint-disable-next-line
+              } AIESEC Canada’s National Leadership Development Conference is an annual 5-day event that brings together 300 business leaders from 30+ universities across Canada to network and develop their skills in teamwork, sales, finance, and marketing. In 2019, we are introducing the Innovation Series to connect our delegates with the leading businesses in technology and disruption.
+            </EventDescription>
           </DescriptionWrapper>
-        </EventDescription> */}
-      </ColumnRight>
-    </Wrapper>
-  </EventDetailsSection>
+        </StyledRow>
+      </ConferenceDescription>
+      <StyledRow>
+        <Col xs="12" sm="10" lg={{ size: 6, offset: 6 }}>
+          <EventInfoTitle>
+            When
+          </EventInfoTitle>
+          <EventDescription>
+            <AddToCalendar />
+          </EventDescription>
+        </Col>
+      </StyledRow>
+      <StyledRow>
+        <Col xs="12" sm="10" lg={{ size: 6, offset: 6 }}>
+          <EventInfoTitle>
+            Where
+          </EventInfoTitle>
+          <div>
+            <EventDescription>
+              SAIT (Southern Alberta Institute of Technology), Calgary, Alberta
+              <AddressLink href="https://www.google.com/maps/place/SAIT/@51.0640269,-114.0907061,17z/data=!3m1!4b1!4m5!3m4!1s0x53716f9268a69e81:0xb1db5e68bd61e28!8m2!3d51.065755!4d-114.0897771" target="_blank" rel="noopener noreferrer">
+                1301-16 Avenue NW Calgary AB, T2M OL4
+              </AddressLink>
+            </EventDescription>
+          </div>
+        </Col>
+      </StyledRow>
+    </Container>
+  </section>
 );
 
 export default EventDetails;
