90d993e1397632de398fc10fe17a8eb2da196fb4
Jackie Luc <jackieluc17@gmail.com>
2018-12-25 22:35:29 -0700

    styles.js - Add shadow variables

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -6,6 +6,9 @@ const variables = {
   hoverColour: '#4EA8FD',
   shadow: `box-shadow: 0 6px 20px rgba(75, 97, 141, 0.3);
           -webkit-box-shadow: 0 6px 20px rgba(75, 97, 141, 0.3);`,
+  blueShadow: `box-shadow: 0 6px 20px rgba(3, 126, 243, 0.3);
+              -webkit-box-shadow: 0 6px 20px rgba(3, 126, 243, 0.3);`,
+  highlight: 'box-shadow: white 0px -0.105em inset, #FFCE33 0px -0.65em inset;',
 };
 
 export default variables;
