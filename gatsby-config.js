683d7e3fe53d394e99c177c335219ee413e16c1b
Jackie Luc <jackieluc17@gmail.com>
2019-01-22 22:43:57 -0700

    init gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `NLDC 2019 | AIESEC`,
+    description: `Join us in "Disrupting the Now!" May 1 - 5, 2019 at the National Leadership Development Conference in Calgary.`,
+    author: `@jackieluc`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#037EF3`,
+        display: `minimal-ui`,
+        icon: `src/images/nldc2019-logo-black.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
