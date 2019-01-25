daa4d71c063de75b781f9b064d8e940dbdcb073e
Jackie Luc <jackieluc17@gmail.com>
2019-01-25 00:18:14 -0700

    FAQ Page - update styling on mobile

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Container } from 'reactstrap';
+import Media from '../utils/media';
 import Styles from '../utils/styles';
 import QuestionsAndAnswers from '../components/Faq/QuestionsAndAnswers';
 
@@ -20,6 +21,16 @@ const Title = styled.h1`
   font-size: 2.5rem;
   text-transform: uppercase;
   margin-bottom: 2rem;
+`;
+
+const StretchedContainer = styled(Container)`
+  padding-right: 0 !important;
+  padding-left: 0 !important;
+
+  ${Media.tablet`
+    padding-right: 15px !important;
+    padding-left: 15px !important;
+  `}
 `;
 
 const FAQ = () => (
@@ -47,12 +58,12 @@ const FAQ = () => (
         <meta name="twitter:description" content="Join us in &#34;Disrupting the Now!&#34; May 1&ndash;5, 2019 at the National Leadership Development Conference in Calgary." />
         <meta name="twitter:image" content="https://www.nldcnow.com/static/media/nldc2019-logo-black.png" />
       </Helmet>
-      <Container>
+      <StretchedContainer>
         <Title>
           Frequently Asked Questions
         </Title>
         <QuestionsAndAnswers />
-      </Container>
+      </StretchedContainer>
     </Wrapper>
   </Layout>
 );
