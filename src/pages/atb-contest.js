a6711277bc8e4f93b104d304a0f3c2406c433890
Jackie Luc <jackieluc17@gmail.com>
2019-02-17 01:13:42 -0800

    ATB Contest | atb logo, updated links to terms & connections

--- src/pages/atb-contest.js
+++ src/pages/atb-contest.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/layout';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
@@ -15,6 +17,7 @@ const Wrapper = styled.div`
   ${Media.laptop`flex-direction: row;`}
   justify-content: center;
   ${Media.laptop`padding-top: 4rem;`}
+  font-size: 14px;
 
   a {
     text-decoration: underline;
@@ -28,7 +31,6 @@ const Title = styled.h1`
   }
   text-align: center;
   font-size: 2rem;
-  ${Media.bigPhone`font-size: 2.5rem;`}
   text-transform: uppercase;
   margin: 2rem 0;
 `;
@@ -55,6 +57,11 @@ const Half = styled.div`
   }
 `;
 
+const Highlight = styled.span`
+  ${Styles.highlight};
+  font-weight: bold;
+`;
+
 const StyledHeader = styled.div`
   margin: 2rem 0;
 
@@ -73,7 +80,7 @@ const ContestDescription = styled.div`
 const EligibilityDescription = styled.div`
   font-size: 12px;
 
-  a {
+  p {
     padding-left: 1rem;
   }
 
@@ -98,86 +105,106 @@ const SubList = styled.ul`
   margin-bottom: 1rem;
 `;
 
-const ATBContest = () => (
-  <Layout noFooter>
-    <Wrapper>
-      <Helmet>
-        <title>
-          ATB Contest | NLDC 2019
-        </title>
-        <link rel="canonical" href="https://www.nldcnow.com/atb-contest" />
-        <meta name="description" content="I am a Transformer Contest. Calling all transformers... Attend the National Leadership Development Conference 2019... on ATB!" />
-        <meta name="keywords" content="nldc 2019 atb contest, atb contest, nldc atb contest, aiesec atb contest, contest, atb, free, flights" />
-        <meta property="og:title" content="ATB Contest | NLDC 2019" />
-        <meta property="og:type" content="website" />
-        <meta property="og:description" content="I am a Transformer Contest. Calling all transformers... Attend the National Leadership Development Conference 2019... on ATB!" />
-        <meta property="og:url" content="https://www.nldcnow.com/atb-contest" />
-        <meta property="og:site_name" content="ATB Contest | NLDC 2019" />
-        <meta property="og:image" content={`https://www.nldcnow.com${Logo}`} />
-        <meta property="fb:app_id" content="594354454320006" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://www.nldcnow.com/atb-contest" />
-        <meta name="twitter:title" content="ATB Contest | NLDC 2019" />
-        <meta name="twitter:description" content="I am a Transformer Contest. Calling all transformers... Attend the National Leadership Development Conference 2019... on ATB!" />
-        <meta name="twitter:image" content={`https://www.nldcnow.com${Logo}`} />
-      </Helmet>
-      <Half gradient>
-        <StyledHeader>
-          <Title gradient>I am a Transformer Contest</Title>
-          <p>Calling all transformers...</p>
-          <p>Attend the National Leadership Development Conference 2019... on ATB!</p>
-        </StyledHeader>
-        <ContestDescription>
-          <p style={{ marginBottom: '1rem' }}>
-            Consistently named one of Canada’s top employers, <a href="https://www.atb.com/careers/Pages/default.aspx" target="_blank" rel="noopener noreferrer">ATB Financial</a> is actively <a href="http://atbalphabeta.com" target="_blank" rel="noopener noreferrer">disrupting banking</a> in order deliver happiness to Albertans, and the world. Within ATB’s 5,300 person strong team, you’ll find 1,000 dedicated transformers working at the forefront of technology, data, CX, AI, blockchain and everything in between.
-          </p>
-          <p style={{ marginBottom: '2rem' }}>
-            ATB invests in its transformers, future and current. If you’re a transformer, then enter ATB’s “I am a Transformer” contest in order to win a chance to have your NLDC delegate fee covered. ATB is always on the search for those that live and breathe our mission and culture. When we find them, we invest in them. Here’s your chance for ATB to invest in you as you continue your leadership journey by attending the NLDC - on us. There are 10 spots up for grabs, valued at $400... enter now!
-          </p>
-          <EligibilityDescription>
-            <h4>Eligibility Requirements</h4>
-            <OrderedList eligibility>
-              <li>Must be a member in good standing of AIESEC Canada and registered for post secondary at a Canadian Institution.</li>
-              <li>18 years or older</li>
-              <li>Must be in the fields of innovation, computer science/business technology, math, engineering, or related.</li>
-            </OrderedList>
-            <a href="/" target="_blank" rel="noopener noreferrer">Click here for full contest terms & conditions.</a>
-          </EligibilityDescription>
-        </ContestDescription>
-      </Half>
-      <Half>
-        <Title>How to enter</Title>
-        <ContestDescription>
-          <OrderedList>
-            <li>Watch the <a href="https://alphabeta.atb.com/careers" target="_blank" rel="noopener noreferrer">Transformation anthem</a> on ATB alphaBeta and get to know the transformation manifesto.</li>
-            <li>Answer the following questions using a social media post, funny meme, boomerang, &lt;90 second video or any creative means:</li>
-            <SubList>
-              <li>How do you live the transformation manifesto every day?</li>
-              <li>What excites you about ATB & transformation</li>
-              <li>How will the NLDC help you continue your journey as a transformative leader?</li>
-            </SubList>
-            <li>Follow us on Instagram and tag us:</li>
-            <SubList>
-              <li>@ATBfinancial</li>
-              <li>@aieseccanadanldc</li>
-              <li>@aiesecincanada</li>
-            </SubList>
-            <li>Make your post public and use the following hashtags:</li>
-            <SubList>
-              <li>#ATBTransforms</li>
-              <li>#ATBListens</li>
-              <li>#DisruptingTheNow</li>
-              <li>#NLDC2019</li>
-            </SubList>
-            <li>Make sure we don’t miss your entry - send us an email at <a href="mailto:alphabeta@atb.com">alphabeta@atb.com</a> with your full name, public link to contest entry, telephone and email for contest communication purposes only.</li>
-          </OrderedList>
-          <p>
-            A Panel of 4 Judges (2 from ATB, 2 from AIESEC Calgary) will review each entry for eligibility, creativity, completeness and applicability of response. 10 of those entries will be randomly chosen for free registration to NLDC (valued at $400).
-          </p>
-        </ContestDescription>
-      </Half>
-    </Wrapper>
-  </Layout>
+const PageQuery = graphql`
+  query {
+    logo: file(relativePath: { eq: "atb-listens.png" }) {
+      childImageSharp {
+        fixed(width: 265) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
+
+const ATBContest = (props) => (
+  <StaticQuery
+    query={PageQuery}
+    render={data => (
+      <Layout noFooter>
+        <Wrapper>
+          <Helmet>
+            <title>
+              ATB Contest | NLDC 2019
+            </title>
+            <link rel="canonical" href="https://www.nldcnow.com/atb-contest" />
+            <meta name="description" content="I am a Transformer Contest. Calling all transformers... Attend the National Leadership Development Conference 2019... on ATB!" />
+            <meta name="keywords" content="nldc 2019 atb contest, atb contest, nldc atb contest, aiesec atb contest, contest, atb, free, flights" />
+            <meta property="og:title" content="ATB Contest | NLDC 2019" />
+            <meta property="og:type" content="website" />
+            <meta property="og:description" content="I am a Transformer Contest. Calling all transformers... Attend the National Leadership Development Conference 2019... on ATB!" />
+            <meta property="og:url" content="https://www.nldcnow.com/atb-contest" />
+            <meta property="og:site_name" content="ATB Contest | NLDC 2019" />
+            <meta property="og:image" content={`https://www.nldcnow.com${Logo}`} />
+            <meta property="fb:app_id" content="594354454320006" />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:url" content="https://www.nldcnow.com/atb-contest" />
+            <meta name="twitter:title" content="ATB Contest | NLDC 2019" />
+            <meta name="twitter:description" content="I am a Transformer Contest. Calling all transformers... Attend the National Leadership Development Conference 2019... on ATB!" />
+            <meta name="twitter:image" content={`https://www.nldcnow.com${Logo}`} />
+          </Helmet>
+          <Half gradient>
+            <StyledHeader>
+              <Title gradient>I am a Transformer Contest</Title>
+              <p>Calling all transformers...</p>
+              <p>Attend the National Leadership Development Conference 2019... on ATB!</p>
+            </StyledHeader>
+            <ContestDescription>
+              <p style={{ marginBottom: '1rem' }}>
+                Consistently named one of Canada’s top employers, <a href="https://www.atb.com/careers/Pages/default.aspx" target="_blank" rel="noopener noreferrer">ATB Financial</a> is actively <a href="http://atbalphabeta.com" target="_blank" rel="noopener noreferrer">disrupting banking</a> in order deliver happiness to Albertans, and the world. Within ATB’s 5,300 person strong team, you’ll find 1,000 dedicated transformers working at the forefront of technology, data, CX, AI, blockchain and everything in between.
+              </p>
+              <p style={{ marginBottom: '2rem' }}>
+                ATB invests in its transformers, future and current. If you’re a transformer, then enter ATB’s “I am a Transformer” contest in order to win a chance to have your NLDC delegate fee covered. ATB is always on the search for those that live and breathe our mission and culture. When we find them, we invest in them. Here’s your chance for ATB to invest in you as you continue your leadership journey by attending the NLDC - on us. There are 10 spots up for grabs, valued at $400... enter now!
+              </p>
+              <EligibilityDescription>
+                <h4>Eligibility Requirements</h4>
+                <OrderedList eligibility>
+                  <li>Must be a member in good standing of AIESEC Canada and registered for post secondary at a Canadian Institution.</li>
+                  <li>18 years or older</li>
+                  <li>Must be in the fields of innovation, computer science/business technology, math, engineering, or related.</li>
+                </OrderedList>
+                <p>Click <a href="https://docs.google.com/document/d/1YuQa8JHHKwqHI-OlmxT69AqQWYtVSt6FKTQ_En_VMKw/edit?fbclid=IwAR3BVSn0G8knXNSePGjN1jUCG58xAW4th0vf9VebtHbjGXqBtbSf1J719Sc" target="_blank" rel="noopener noreferrer">here</a> for full contest terms & conditions.</p>
+              </EligibilityDescription>
+            </ContestDescription>
+          </Half>
+          <Half>
+            <Title>How to enter</Title>
+            <ContestDescription>
+              <OrderedList>
+                <li>Watch the <a href="https://alphabeta.atb.com/careers" target="_blank" rel="noopener noreferrer">Transformation anthem</a> on ATB alphaBeta and get to know the transformation manifesto.</li>
+                <li>Answer the following questions using a social media post, funny meme, boomerang, &lt;90 second video or any creative means:</li>
+                <SubList>
+                  <li>How do you live the transformation manifesto every day?</li>
+                  <li>What excites you about ATB & transformation</li>
+                  <li>How will the NLDC help you continue your journey as a transformative leader?</li>
+                </SubList>
+                <li>Follow us on Instagram and tag us:</li>
+                <SubList>
+                  <li>@ATBfinancial</li>
+                  <li>@aieseccanadanldc</li>
+                  <li>@aiesecincanada</li>
+                </SubList>
+                <li>Make your post public and use the following hashtags:</li>
+                <SubList>
+                  <li>#ATBTransforms</li>
+                  <li>#ATBListens</li>
+                  <li>#DisruptingTheNow</li>
+                  <li>#NLDC2019</li>
+                </SubList>
+                <li>Make sure we don’t miss your entry - send us an email at <a href="mailto:alphabeta@atb.com">alphabeta@atb.com</a> with your full name, public link to contest entry, telephone and email for contest communication purposes only.</li>
+              </OrderedList>
+              <p>
+                A Panel of 4 Judges (2 from ATB, 2 from AIESEC Calgary) will review each entry for eligibility, creativity, completeness and applicability of response. 10 of those entries will be randomly chosen for <Highlight>free</Highlight> registration to NLDC (valued at $400).
+              </p>
+            </ContestDescription>
+            <a href="https://www.atb.com/Pages/default.aspx" target="_blank" rel="noopener noreferrer" style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
+              <Img fixed={data.logo.childImageSharp.fixed} alt="ATB Listens logo" />
+            </a>
+          </Half>
+        </Wrapper>
+      </Layout>
+    )}
+  />
 );
 
 export default ATBContest;
