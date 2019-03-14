de86a18c5adbec850f9fb07332ef512af9c680e3
Jackie Luc <jackieluc17@gmail.com>
2019-03-13 22:54:22 -0600

    Testimonials | Added air quotes style

--- src/components/YouthToBusiness/Testimonials.js
+++ src/components/YouthToBusiness/Testimonials.js
@@ -30,7 +30,7 @@ const StyledCarousel = styled(Carousel)`
 `;
 
 const StyledCarouselItem = styled(CarouselItem)`
-  width: 900px;
+  width: 100%;
   height: 300px;
   display: flex !important;
   opacity: 0;
@@ -90,6 +90,34 @@ const items = [
   }
 ];
 
+const StyledSvg = styled.svg`
+  position: absolute;
+  z-index: 2;
+  width: 60px;
+  height: 60px;
+  top: 90px;
+  right: 0;
+  -webkit-transform: scale(-1, -1);
+  -o-transform: scale(-1, -1);
+  -ms-transform: scale(-1, -1);
+  transform: scale(-1, -1);
+  enable-background: new 0 0 512.5 512.5
+`;
+
+const QuotesBackground = () => (
+  <StyledSvg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+    width="512.5px" height="512.5px" viewBox="0 0 512.5 512.5">
+    <g>
+      <path d="M112.5,208.25c61.856,0,112,50.145,112,112s-50.144,112-112,112s-112-50.145-112-112l-0.5-16
+        c0-123.712,100.288-224,224-224v64c-42.737,0-82.917,16.643-113.137,46.863c-5.817,5.818-11.126,12.008-15.915,18.51
+        C100.667,208.723,106.528,208.25,112.5,208.25z M400.5,208.25c61.855,0,112,50.145,112,112s-50.145,112-112,112
+        s-112-50.145-112-112l-0.5-16c0-123.712,100.287-224,224-224v64c-42.736,0-82.918,16.643-113.137,46.863
+        c-5.818,5.818-11.127,12.008-15.916,18.51C388.666,208.723,394.527,208.25,400.5,208.25z"
+      />
+    </g>
+  </StyledSvg>
+);
+
 class Testimonials extends Component {
   constructor(props) {
     super(props);
@@ -146,6 +174,7 @@ class Testimonials extends Component {
 
     return (
       <Wrapper>
+        <QuotesBackground />
         <StyledCarousel activeIndex={activeIndex} next={this.next} previous={this.previous} interval="1000000">
           <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
             { slides }
